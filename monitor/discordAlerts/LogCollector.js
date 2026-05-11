43fcd519d08a6e36d12c9d894c6f213b32441d05
YICHUNLIN <yichun03@gmail.com>
2026-05-11 11:55:10 +0800

    fix logCollector

--- monitor/discordAlerts/LogCollector.js
+++ monitor/discordAlerts/LogCollector.js
@@ -9,17 +9,18 @@ function Method(context){
     this.webhookurl = 'https://discord.com/api/webhooks/1501949307316863078/Xxfusv45TYI6ie-Cq5U_NcEoT6S6aqSNxe7nZ0VNiRG44g6_6pje3cAWGKfDe0h0C_Z4';
 }
 
-Method.prototype.exec = async function({ip, alertBlocks, level}){
+Method.prototype.exec = async function({ip, alerts, level}){
     // 如果是嚴重等級 (Critical)，可以在 content 加上 @everyone 或特定身份組 ID
     const pingText = level === 'critical' ? '@everyone 🚨 伺服器發生嚴重錯誤！' : '';
 
     try {
-        if (!Array.isArray(alertBlocks)) throw new Error('[USE_ERORR][PARAMETER] message must be an array')
+        console.log(alerts)
+        if (!Array.isArray(alerts)) throw new Error('[USE_ERORR][PARAMETER] message must be an array')
         const payload = {
             content: pingText, // 顯示在訊息最上方的純文字
             embeds: [{
                 title: `🔔 伺服器監控報告 | 來源 IP: ${ip}`,
-                description: alertBlocks.join('\n\n'),
+                description: alerts.join('\n\n'),
                 color: colors[level] || colors.warning,
                 footer: { 
                     text: "Log Analyzer Monitor" 
